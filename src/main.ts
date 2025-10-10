3cff4ff1a3a43ea00d1fb829e9c9c6b49fa0d146
Justin <mariajustin444@gmail.com>
2025-10-10 17:45:27 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Listening on port', process.env.PORT );
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for my NestJS project')
+    .setVersion('1.0')
+    .addBearerAuth() // optional, adds JWT Auth button
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); 
+  console.log('Listening on port', process.env.PORT ?? 3000);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
